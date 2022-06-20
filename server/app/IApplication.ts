7fe74b8d915c72eeda2b87eda65e50dd19b06967
Bartosz <b.pazdur@yahoo.com>
2022-06-20 13:19:28 +0200

    Bug fixing and improvements
    Improve application loading

--- server/app/IApplication.ts
+++ server/app/IApplication.ts
@@ -1,7 +1,5 @@
 import Application from "@/Application";
 
 export interface IApplication {
-
-  
   run(app: Application): Promise<void>;
 }
--- server/server.ts
+++ server/server.ts
@@ -1,2 +1,21 @@
-// code ..
-console.log('init code');
+import 'reflect-metadata'
+import { Container } from 'inversify';
+import Application from '@/Application';
+import { Kernel } from '@/Kernel';
+import { IApplication } from '@/IApplication';
+
+const container = new Container({
+  defaultScope: 'Singleton',
+});
+
+container.bind<IApplication>('IApplication').to(Kernel);
+
+async function bootstrap() {
+  const app = new Kernel();
+  await app.run(new Application());
+
+  return app
+}
+
+bootstrap()
+
