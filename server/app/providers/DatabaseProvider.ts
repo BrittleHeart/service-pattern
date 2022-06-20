74a13f8da42c3ac6fded08afb897a086000a5e2b
Bartosz <b.pazdur@yahoo.com>
2022-06-20 20:27:57 +0200

    Bug fixing
    Bug fixing and improvements

--- server/app/providers/DatabaseProvider.ts
+++ server/app/providers/DatabaseProvider.ts
@@ -1,25 +1,16 @@
-import mongoose, { mongo, MongooseError } from 'mongoose'
+import mongoose, { mongo, MongooseError } from "mongoose";
 import IDatabaseConfig from "@/providers/IDatabaseConfig";
 
 export default abstract class DatabaseProvider {
-  private readonly _connectionConfig: IDatabaseConfig
-  protected databaseContext: Promise<void | typeof mongoose>;
-  
+  private readonly _connectionConfig: IDatabaseConfig;
+
   constructor(connectionConfig: IDatabaseConfig) {
     this._connectionConfig = connectionConfig;
-
-    this.databaseContext = mongoose.connect(connectionConfig.uri, {
-      autoIndex: true,
-      maxConnecting: 5
-    }).then(connect => connect)
   }
 
-  public async connect(): Promise<void | typeof mongoose> {
-    try {
-      console.log('Works');
-      return await this.databaseContext
-    } catch(error) {
-      console.log(error);
-    }
+  public async connect() {
+    return await mongoose.connect("mongodb://localhost:27017/serverService", {
+      autoIndex: true,
+    })
   }
 }
