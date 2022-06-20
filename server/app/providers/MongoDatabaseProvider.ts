bd47c01e8ba08da20263d151c259ca66a60605e8
Bartosz <b.pazdur@yahoo.com>
2022-06-20 15:49:13 +0200

    Code clarity
    Fix code clarity

--- server/app/providers/MongoDatabaseProvider.ts
+++ server/app/providers/MongoDatabaseProvider.ts
@@ -1,8 +1,6 @@
-import { injectable } from "inversify";
 import DatabaseProvider from "@/providers/DatabaseProvider";
 import IDatabaseConfig from "./IDatabaseConfig";
 
-@injectable()
 export default class MongoDatabaseProvider extends DatabaseProvider {
   constructor(connectionConfig: IDatabaseConfig) {
     super(connectionConfig);
